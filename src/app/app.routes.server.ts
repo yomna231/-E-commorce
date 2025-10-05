e4600859e108e772a998b3c9f3edc0f5f37fe7ac
yomna231 <yomnaayman2111@example.com>
2025-10-05 11:50:11 +0300

    commit

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,33 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
+
 export const serverRoutes: ServerRoute[] = [
+
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'checkout/:cartId',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'order-confirmation/:id',
+    renderMode: RenderMode.Client,
+  },
+  // {
+  //   path: 'orderConfirmation/*',
+  //   renderMode: RenderMode.Client,
+  // },
+  {
+    path: 'checkout/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'single/:pid/:pName',
+    renderMode: RenderMode.Client,
+  },
+  
+  {
+   path: '**',
+    renderMode:  RenderMode.Prerender
+  
+  },
+
 ];
